db7edf84905ddfc8af380c2f4ff51b3f1c255e2e
Michael Netzer <netzermi@gmail.com>
2023-06-05 09:51:39 +0200

    Initial commit

---
+++ src/at/hakimst/apr/StatUtil.java
@@ -0,0 +1,11 @@
+package at.hakimst.apr;
+
+import java.util.ArrayList;
+
+public class StatUtil {
+
+    public static double getMax(ArrayList<Double> values){
+        //TODO
+        return 0;
+    }
+}
